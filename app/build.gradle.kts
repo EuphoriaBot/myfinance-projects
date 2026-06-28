5cd4b75fd84a2604643b22d12a664b2ea74d6db3
EuphoriaBot <dimasfirja80@gmail.com>
2026-06-28 11:07:36 +0800

    Fix Gradle Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.myfinance"
-    compileSdk = 37
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.myfinance"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
