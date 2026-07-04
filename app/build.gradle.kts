c2a2c4687c1f384912447fe8ae3af4ddb2b70625
EuphoriaBot <dimasfirja80@gmail.com>
2026-07-04 19:24:08 +0800

    Update Build Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
     ksp("androidx.room:room-compiler:2.6.1")
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
