e68cbc1b3ed6f1f4fd36c12f3525684e396f44f6
EuphoriaBot <dimasfirja80@gmail.com>
2026-07-20 17:36:39 +0800

    Update AppApplication

--- app/src/main/java/com/example/myfinance/AppApplication.kt
+++ app/src/main/java/com/example/myfinance/AppApplication.kt
@@ -1,11 +1,22 @@
 package com.example.myfinance
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class AppApplication : Application() {
+class AppApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 
     override fun onCreate() {
         super.onCreate()
