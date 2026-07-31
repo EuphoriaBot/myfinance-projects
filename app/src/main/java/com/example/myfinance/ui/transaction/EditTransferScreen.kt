cc48e6bb4842502e3da5cadbc91438cc7fa05d86
EuphoriaBot <dimasfirja80@gmail.com>
2026-07-31 17:04:44 +0800

    Add EditTransferScreen

---
+++ app/src/main/java/com/example/myfinance/ui/transaction/EditTransferScreen.kt
@@ -0,0 +1,12 @@
+package com.example.myfinance.ui.transaction
+
+import androidx.compose.runtime.Composable
+import com.example.myfinance.data.local.entity.TransactionEntity
+
+@Composable
+fun EditTransferScreen(
+    transaction: TransactionEntity,
+    onDismiss: () -> Unit
+) {
+
+}
